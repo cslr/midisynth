22afa7a6f839fa675eac471451bc298d3b517b6b
Tomas Ukkonen <nop@iki.fi>
2017-02-15 01:37:17 +0200

    Planned midi synthesizer framework documentation diagram.

--- rbm_test.cpp
+++ rbm_test.cpp
@@ -7,9 +7,9 @@
 
 int main(int argc, char** argv)
 {
-  printf("GB-RBM MIDI NOTES TEST\n");
+  printf("RBM MIDI NOTES TEST\n");
   
-  const int HIDDEN = 10; // number of hidden nodes in GB-RBM network
+  const int HIDDEN = 10; // number of hidden nodes in RBM networks
 
   
   // loads examples database
@@ -29,45 +29,80 @@ int main(int argc, char** argv)
     printf("ERROR: Bad database.\n");
     return -1;
   }
+
+  printf("%d samples training samples loaded\n", db.size(0));
+
+  std::vector<unsigned int> inputArch, outputArch;
+
+  inputArch.push_back(db.dimension(0));
+  inputArch.push_back(db.dimension(0)*2);
+  inputArch.push_back(HIDDEN);
+
+  outputArch.push_back(db.dimension(1));
+  outputArch.push_back(db.dimension(1)*2);
+  outputArch.push_back(HIDDEN);
   
-  whiteice::GBRBM<> gbrbm;
+  whiteice::DBN<> inputRBM(inputArch);
+  whiteice::DBN<> outputRBM(outputArch);
   
-  if(gbrbm.resize(db.dimension(0)+db.dimension(1), HIDDEN) == false){
-    printf("ERROR: GBRBM internal error.\n");
-    return -1;
-  }
-  
-  std::vector< whiteice::math::vertex<> > samples;
+  std::vector< whiteice::math::vertex<> > input_samples;
+  std::vector< whiteice::math::vertex<> > output_samples;
   
   for(unsigned int i=0;i<db.size(0);i++){
-    whiteice::math::vertex<> v(db.dimension(0)+db.dimension(1));
-    if(v.write_subvertex(db.access(0, i), 0) == false || 
-       v.write_subvertex(db.access(1, i), db.dimension(0)) == false){
-      printf("Exporting datapoint %d/%d failed.\n", i, db.size(0));
-      continue;
-    }
-    
-    samples.push_back(v);
+    input_samples.push_back(db.access(0,i));
+    output_samples.push_back(db.access(1,i));
   }
-  
-  if(gbrbm.initializeWeights() == false){
-    printf("ERROR: GBRBM internal error.\n");
-    return -1;
-  }
+
+  inputRBM.initializeWeights();
+  outputRBM.initializeWeights();
   
   //////////////////////////////////////////////////////////////////////
   // now we learn GB-RBM weights
   
-  printf("GBRBM architecture: %dx%d\n",
-	 gbrbm.getVisibleNodes(), gbrbm.getHiddenNodes());
+  printf("INPUT architecture: %dx%dx%d\n",
+	 inputArch[0], inputArch[1], inputArch[2]);
   
-  printf("Learning GBRBM weights (gradient descent data maximum likelihood)..\n");
+  printf("Learning RBM weights (gradient descent data maximum likelihood)..\n");
   fflush(stdout);
+
+  whiteice::math::blas_real<float> dW = 0.0001;
+
+  inputRBM.learnWeights(input_samples, dW, true);
+
+  whiteice::nnetwork<>* net = nullptr;
+
+  if(inputRBM.convertToNNetwork(net) == true){
+    if(net->save("midi-input-nnetwork.conf") == false){
+      printf("ERROR: saving MIDI input RBM network failed\n");
+    }
+    
+    delete net;
+    net = nullptr;
+  }
+  else{
+    printf("ERROR: converting RBM to nnetwork failed,\n");
+  }
+
+  printf("OUTPUT architecture: %dx%dx%d\n",
+	 outputArch[0], outputArch[1], outputArch[2]);
   
-  auto error = gbrbm.learnWeights(samples, 500, true);
-  
-  printf("GBRBM optimization finished. Reconstruction error: %f\n", error.c[0]);
+  printf("Learning RBM weights (gradient descent data maximum likelihood)..\n");
   fflush(stdout);
+
+  outputRBM.learnWeights(output_samples, dW, true);
+
+  if(outputRBM.convertToNNetwork(net) == true){
+    if(net->save("midi-output-nnetwork.conf") == false){
+      printf("ERROR: saving MIDI output RBM network failed\n");
+    }
+    
+    delete net;
+    net = nullptr;
+  }
+  else{
+    printf("ERROR: converting RBM to nnetwork failed,\n");
+  }
+
   
   return 0;
 }
